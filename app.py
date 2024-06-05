389b5e477eaf2afca04750d695c956619d52d346
visw20 <162998373+visw20@users.noreply.github.com>
2024-06-05 13:20:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,22 @@
 import streamlit as st
+import pandas as pd
 
-st.write("Hello world")
+def upload_file():
+    st.title("CSV File Uploader")
 
+    # File uploader widget
+    uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+
+    # Check if a file is uploaded
+    if uploaded_file is not None:
+        st.write("Uploaded file:", uploaded_file.name)
+
+        # Display CSV file content using pandas
+        df = pd.read_csv(uploaded_file)
+        st.dataframe(df)
+    else:
+        st.write("Please upload a CSV file.")
+
+def main():
+    upload_file()
 
